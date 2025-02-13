9822fc609aa85c0dcfaca2e7e18b5774e4909b5b
renatomayoral <renatomayoral@gmail.com>
2025-02-13 11:43:57 -0300

    Add Prisma setup, fetch stablecoins workflow, and update layout metadata; define new data types for stablecoins and coins

--- config/types.ts
+++ config/types.ts
@@ -15,3 +15,67 @@ export type Payment = {
     amount: number;
     skynet_score: number;
   };
+
+  export type Platform = {
+    id: number;
+    name: string;
+    symbol: string;
+    slug: string;
+    token_address: string;
+  }
+  
+  export type Quote = {
+    id: number;
+    price: number;
+    volume_24h: number;
+    volume_change_24h: number;
+    percent_change_1h: number;
+    percent_change_24h: number;
+    percent_change_7d: number;
+    percent_change_30d: number;
+    percent_change_60d: number;
+    percent_change_90d: number;
+    market_cap: number;
+    market_cap_dominance: number;
+    fully_diluted_market_cap: number;
+    last_updated: string;
+  }
+  
+  export type Coin = {
+    id: number;
+    category_id: string;
+    name: string;
+    symbol: string;
+    slug: string;
+    num_market_pairs: number;
+    date_added: string;
+    tags: string[];
+    max_supply: number | null;
+    circulating_supply: number;
+    total_supply: number;
+    platform: Platform;
+    is_active: number | null | undefined;
+    infinite_supply: boolean;
+    cmc_rank: number;
+    is_fiat: number;
+    last_updated: string;
+    quote: {
+      USD: Quote;
+    };
+  }
+  
+  export type StablecoinData = {
+    id: string;
+    name: string;
+    title: string;
+    description: string;
+    num_tokens: number;
+    last_updated: string;
+    avg_price_change: number;
+    market_cap: number;
+    market_cap_change: number;
+    volume: number;
+    volume_change: number;
+    coins: Coin[];
+  }
+  
